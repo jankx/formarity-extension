2c5daa0512737c5eab402d5fbcb1fff749818ecc
Puleeno Nguyen <puleeno@gmail.com>
2026-03-17 10:33:46 +0700

    feat: initial port of Formarity plugin core

---
+++ formarity.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Formarity Extension entry point
+ *
+ * This file serves as a metadata anchor so that WordPress helper functions
+ * like plugin_dir_path() / plugin_dir_url() can resolve the correct
+ * extension directory path and URL when passed this file's path.
+ *
+ * The actual bootstrap logic lives in FormarityExtension.php.
+ * Do NOT add any executable code here.
+ *
+ * @wordpress-plugin
+ * Plugin Name:  Formarity
+ * Plugin URI:   https://formello.net
+ * Description:  Lightweight Gutenberg contact form builder (Jankx Extension).
+ * Version:      2.7.0
+ * Author:       Tropicalista
+ * Author URI:   https://www.francescopepe.com/
+ * License:      GPL-2.0+
+ * License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:  formello
+ * Domain Path:  /languages
+ */
+
+defined('ABSPATH') || exit;
